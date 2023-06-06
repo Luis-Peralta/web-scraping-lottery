f69d1c052a8cb2750f4bbcac186bbd27cf760406
Luis-Peralta <luis_peralta_000@hotmail.com>
2023-06-06 12:32:25 -0300

    validation extra and fix bug to obtain all data added

--- script/extractAll.js
+++ script/extractAll.js
@@ -20,8 +20,6 @@ const regexNumber = /[0-9]{1,2}/gm;
   console.log('\x1b[36mObtaining data...\x1b[0m');
   //data:::
   const allResults = [];
-  const objectResult = {};
-  objectResult.results = {};
 
   //puppet config:::
   const browser = await puppeteer.launch({ headless: 'new', defaultViewport: null, args:['--start-maximized' ] });
@@ -32,6 +30,7 @@ const regexNumber = /[0-9]{1,2}/gm;
   //save all data:::disable by default
   if(process.env.SAVE_DATA_ALL.toLowerCase() === 'true') {
     for (let index = 0; index < 10; index++) {
+      const objectResult = { results: {} };
       await page.waitForSelector(iconPlus);
       const sorteo = await page.$$eval(itemsLeft, texts => { return texts.map(text => text.textContent); });
       const fecha = await page.$$eval(itemsRight, texts => { return texts.map(text => text.textContent); });
@@ -40,13 +39,14 @@ const regexNumber = /[0-9]{1,2}/gm;
       const results = await page.$$(iconPlus);
       await results[index].evaluate(button => button.click());
   
-      await obtainNumbers();
-  
+      await obtainNumbers(objectResult);
+      
       allResults.push(objectResult);
       await page.goBack();
     }
   } else {
     //save the last results (first element):::
+    const objectResult = { results: {} };
     await page.waitForSelector(iconPlus);
     const sorteo = await page.$eval(firstLeft, text => text.textContent);
     const fecha = await page.$eval(firstRight, text => text.textContent);
@@ -55,13 +55,13 @@ const regexNumber = /[0-9]{1,2}/gm;
     objectResult.fecha = fecha.match(regexFecha)[0];
     await page.click(firstIconPlus);
 
-    await obtainNumbers();
+    await obtainNumbers(objectResult);
 
     allResults.push(objectResult);
   }
 
   //function to obtain the all results for each sorteo
-  async function obtainNumbers() {
+  async function obtainNumbers(objectResult) {
     await page.waitForSelector(tableHeader);
     const nSorteo = await page.$eval(tableHeader, text => text.textContent);
     objectResult.results.numSorteo = parseInt(nSorteo.match(regexSorteo)[0]);
@@ -76,5 +76,5 @@ const regexNumber = /[0-9]{1,2}/gm;
   await browser.close();
   console.log('\x1b[36mScript finished!\x1b[0m');
 
-  process.env.SAVE_DATA.toLowerCase() === 'true' ? run(allResults) : console.log('\x1b[33mdata not sent to MongoDB\x1b[0m');
+  process.env.SAVE_DATA.toLowerCase() === 'true' && allResults.length > 0 ? run(allResults) : console.log('\x1b[33mdata not sent to MongoDB\x1b[0m');
 })();
