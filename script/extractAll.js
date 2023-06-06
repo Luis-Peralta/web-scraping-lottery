3d0872edf2dd56c4138e59c0a2a8554855304100
Luis-Peralta <luis_peralta_000@hotmail.com>
2023-06-05 23:14:07 -0300

    refactor code, dotenv added

--- script/extractAll.js
+++ script/extractAll.js
@@ -1,32 +1,39 @@
+/* eslint-disable no-undef */
 import puppeteer from 'puppeteer';
+import 'dotenv/config';
+
+//const selectors:::
+const table = '.results-list';
+const tableHeader = '.card-header h5';
+const iconPlus = '.results-list__item a';
+const itemsLeft = '[class="results-list__item"] .results-number:nth-child(1)';
+const itemsRight = '[class="results-list__item"] .results-number:nth-child(2)';
 
 (async () => {
   const allResults = [];
   const browser = await puppeteer.launch({ headless: 'new', defaultViewport: null, args:['--start-maximized' ] });
   const page = await browser.newPage();
 
-  await page.goto('https://loteria.chaco.gov.ar/juego/quiniela_poceada');
-  await page.waitForSelector('.results-list');
+  await page.goto(process.env.URL);
+  await page.waitForSelector(table);
 
   for (let index = 0; index < 10; index++) {
     const objectResult = {};
     objectResult.results = {};
-    await page.waitForSelector('.results-list__item a');
-    const sorteo = await page.$$eval('[class="results-list__item"] .results-number:nth-child(1)', texts => { return texts.map(text => text.textContent.match(/[0-9]{1,7}/gm)); });
-    const fecha = await page.$$eval('[class="results-list__item"] .results-number:nth-child(2)', texts => { return texts.map(text => text.textContent.match(/([\d]{2}\/[\d]{2}\/[\d]{2})/gm)); });
+    await page.waitForSelector(iconPlus);
+    const sorteo = await page.$$eval(itemsLeft, texts => { return texts.map(text => text.textContent.match(/[0-9]{1,7}/gm)); });
+    const fecha = await page.$$eval(itemsRight, texts => { return texts.map(text => text.textContent.match(/([\d]{2}\/[\d]{2}\/[\d]{2})/gm)); });
     objectResult.sorteo = parseInt(sorteo[index][0]);
     objectResult.fecha = fecha[index][0];
     
-
-
-    const results = await page.$$('.results-list__item a');
+    const results = await page.$$(iconPlus);
     await results[index].evaluate(button => button.click());
-    await page.waitForSelector('.juego-extractos');
+    await page.waitForSelector(tableHeader);
 
-    const nSorteo = await page.$eval('.card-header h5', text => text.textContent.match(/[0-9]{1,7}/gm));
+    const nSorteo = await page.$eval(tableHeader, text => text.textContent.match(/[0-9]{1,7}/gm));
     objectResult.results.numSorteo = parseInt(nSorteo[0]);
-    const ubicacion = await page.$$eval('[class="results-list__item"] .results-number:nth-child(1)', texts => { return texts.map(text => text.textContent.match(/[0-9]{1,2}/gm)); });
-    const premiados = await page.$$eval('[class="results-list__item"] .results-number:nth-child(2)', texts => { return texts.map(text => text.textContent.match(/[0-9]{1,2}/gm)); });
+    const ubicacion = await page.$$eval(itemsLeft, texts => { return texts.map(text => text.textContent.match(/[0-9]{1,2}/gm)); });
+    const premiados = await page.$$eval(itemsRight, texts => { return texts.map(text => text.textContent.match(/[0-9]{1,2}/gm)); });
     for (let index = 0; index < 10; index++) {
       objectResult.results[`number-${ubicacion[index][0]}`] = parseInt(premiados[index][0]);
     }
