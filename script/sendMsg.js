e1012c0a5f1af0bf025f465001eb3f7c71e7928b
Luis Peralta <luis_peralta_000@hotmail.com>
2024-04-27 12:05:16 -0300

    the phone number of Twilio was updated

--- script/sendMsg.js
+++ script/sendMsg.js
@@ -6,6 +6,19 @@ import { giveLuckyNumbers } from './luckyNumbers.js';
 const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 const date = new Date().toUTCString();
 
+// we need to update the phone number  every so often
+if( date.includes('Thu') || date.includes('Sat') || date.includes('Tue') ) {
+  client.messages
+    .create({
+      body: `Los números de la suerte para la próxima poceada son: ${giveLuckyNumbers().toString().replaceAll(',',', ')}`,
+      from: '+16362384935',
+      to: '+543624747305'
+    })
+    .then(message => console.log(message.sid));
+} else {
+  console.log('Today the lucky numbers don\'t send :( ');
+}
+
 /*
 ******Code to send to multiple numbers !!! DON'T REMOVE*****
 const numbersToMessage = ['+543624747305', '+543624547941'];
@@ -18,15 +31,3 @@ numbersToMessage.forEach(async number => {
   });
   console.log(message.status);
 });*/
-
-if( date.includes('Thu') || date.includes('Sat') || date.includes('Tue') ) {
-  client.messages
-    .create({
-      body: `Los números de la suerte para la próxima poceada son: ${giveLuckyNumbers().toString().replaceAll(',',', ')}`,
-      from: '+13613012013',
-      to: '+543624747305'
-    })
-    .then(message => console.log(message.sid));
-} else {
-  console.log('Today the lucky numbers don\'t send :( ');
-}
