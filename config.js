3148aaaf7e2ed9b9034401bde8aa6b274f718615
Luis Peralta <luis_peralta_000@hotmail.com>
2025-09-27 15:31:36 -0300

    telegram config

--- config.js
+++ config.js
@@ -3,34 +3,22 @@ import process from 'process';
 
 dotenv.config({ override: true });
 
-const { 
-  ALL_DATA, 
-  COLLECTION, 
-  DB_NAME,
-  SAVE_DATA,
-  SEND_AI_ANALYSIS,
-  URL,
-  DB_URI,
-  FROM_NUMBER,
-  GEMINI_API_KEY,
-  TO_NUMBER,
-  TWILIO_ACCOUNT_SID,
-  TWILIO_AUTH_TOKEN,
-  TELEGRAM_BOT_TOKEN,
-} = process.env;
-
+/**
+ * Configuration object containing environment variables
+ * @typedef {Object} Config
+ */
 export default {
-  ALL_DATA,
-  COLLECTION,
-  DB_NAME,
-  SAVE_DATA,
-  SEND_AI_ANALYSIS,
-  URL,
-  DB_URI,
-  FROM_NUMBER,
-  GEMINI_API_KEY,
-  TO_NUMBER,
-  TWILIO_ACCOUNT_SID,
-  TWILIO_AUTH_TOKEN,
-  TELEGRAM_BOT_TOKEN,
+  ALL_DATA: process.env.ALL_DATA ?? '',
+  COLLECTION: process.env.COLLECTION ?? '',
+  DB_NAME: process.env.DB_NAME ?? '',
+  SAVE_DATA: process.env.SAVE_DATA ?? '',
+  SEND_AI_ANALYSIS: process.env.SEND_AI_ANALYSIS ?? 'false',
+  URL: process.env.URL ?? '',
+  DB_URI: process.env.DB_URI ?? '',
+  FROM_NUMBER: process.env.FROM_NUMBER ?? '',
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY ?? '',
+  TO_NUMBER: process.env.TO_NUMBER ?? '',
+  TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID ?? '',
+  TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN ?? '',
+  TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN ?? '',
 };
--- script/services/telegram.js
+++ script/services/telegram.js
@@ -2,6 +2,27 @@ import TelegramBot from 'node-telegram-bot-api';
 import config from '@config';
 
 const token = config.TELEGRAM_BOT_TOKEN;
-
 const bot = new TelegramBot(token, {polling: true});
 
+async function getChatId() {
+  let chatId;
+  bot.on('message', (msg) => {
+    chatId = msg.chat.id;
+    console.log('Message Received', msg.text);
+    console.log('ChatId:', msg.chat.id);
+  });
+  return chatId;
+};
+
+async function sendTelegramMessage(message = '') {
+  try {
+    await bot.sendMessage(config.TELEGRAM_CHAT_ID, message);
+    console.log('Message sent to Telegram successfully');
+  } catch (error) {
+    console.error('Error sending message to Telegram:', error);
+  }
+};
+
+export { sendTelegramMessage };
+
+getChatId();
